e6b8f12698f0d51b87a6d051663e9d98f089a355
libmonsoon-dev <daniil.stepanenko.96@gmail.com>
2018-11-17 20:42:53 +0200

    added commands from bash history

---
+++ deb/desktop/apt-install.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+sudo apt update && \
+sudo apt upgrade && \
+sudo apt install -y \
+	keepassx \
+	ncdu \
+	mc \
+	htop \
+	curl \
+	net-tools \
+	apache2-utils \
+	lazygit \
+	telegram-desktop \
+	gstreamer1.0-tool \
+	chromium-browser \
+	vlc \
+	golang \
+	docker \
+	docker-compose \
+	tree \
+	tmux \
+	python3-pip \
+	xonsh
