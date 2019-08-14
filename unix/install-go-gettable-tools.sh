06bc9f6e1ebbe7763d09ea4ca8eb3f6e31dada85
Daniil <d.stepanenko@noisyminer.com>
2019-08-14 12:37:55 +0300

    unix/install-go-gettable-tools.sh  added

---
+++ unix/install-go-gettable-tools.sh
@@ -0,0 +1,7 @@
+go get -u -v \
+  github.com/jesseduffield/lazygit \
+  github.com/jesseduffield/lazydocker \
+  github.com/pquerna/ffjson \
+  github.com/cheekybits/genny \
+  github.com/gopherjs/gopherjs \
+  golang.org/x/vgo \
