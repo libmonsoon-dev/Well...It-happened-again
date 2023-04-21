a7770de0b982af441d79ac2db922783c913d301e
Daniil Stepanenko <daniil.stepanenko.96@gmail.com>
2023-04-21 08:42:08 +0300

    update deb desctop init script

---
+++ README.md
@@ -0,0 +1,6 @@
+## Debian-like desctop setup:
+
+Add to autostart (probably need to replace ~/ to ```echo /home/$USER`, idk is it working like that)
+```
+    ~/init.sh > /tmp/init.log 2> /tmp/init.err
+```
--- deb/desktop/home/init.sh
+++ deb/desktop/home/init.sh
@@ -1,9 +1,19 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
 echo "Running as ${USER}"
 
 sleep 30
 
-sudo snap refresh firefox && firefox &
+# TODO: move to just firefox and let env manage where is bin actualy located
+/usr/bin/firefox &
 
+sudo snap refresh firefox || echo "snap refresh error"
+sleep 5
+# TODO: remove snap-based firefox when I will fully migrate all tabs to apt-based
+/snap/bin/firefox &
 TZ="Europe/Kiev" teams &
+spotify &
 
-konsole -e tmux attach || tmux &
+# Kubuntu-specific thing, yes
+konsole -e tmux attach || tmux
