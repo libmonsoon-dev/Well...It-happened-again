ab61258439ba4150a38a835578d132b3b020871e
Daniil Stepanenko <daniil.stepanenko.96@gmail.com>
2023-04-25 12:24:09 +0300

    init script updates

--- deb/desktop/home/init.sh
+++ deb/desktop/home/init.sh
@@ -1,19 +1,17 @@
 #!/usr/bin/env bash
 
-set -euxo pipefail
+set -uxo pipefail
 echo "Running as ${USER}"
 
 sleep 30
 
-# TODO: move to just firefox and let env manage where is bin actualy located
 /usr/bin/firefox &
 
 sudo snap refresh firefox || echo "snap refresh error"
 sleep 5
-# TODO: remove snap-based firefox when I will fully migrate all tabs to apt-based
-/snap/bin/firefox &
-TZ="Europe/Kiev" teams &
-spotify &
+/snap/bin/firefox || echo "snap firefox error" &
+TZ="Europe/Kiev" teams || echo "teams error" &
+spotify || "spotify error" &
 
-# Kubuntu-specific thing, yes
-konsole -e tmux attach || tmux
+konsole -e "bash -c 'tmux attach || tmux'" || echo "konsole error" &
+
