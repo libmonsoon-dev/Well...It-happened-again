3c78d1a55c0d4a061a3278c034ca5ae2624c35c4
Daniil Stepanenko <daniil.stepanenko.96@gmail.com>
2024-10-18 18:19:46 +0300

    updated go/setup.sh
    Signed-off-by: Daniil Stepanenko <daniil.stepanenko.96@gmail.com>

---
+++ go/setup.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+source "$(dirname "$0")/../bash.lib.sh"
+
+if [ -z "$(which go)" ]; then
+  open https://go.dev/doc/install
+fi
+
+while [ -z "$(which go)" ]; do
+  echo "go bin not found"
+  read -r
+done
+
+
+COMMANDS=(
+  github.com/restic/restic/cmd/restic
+  github.com/jesseduffield/lazygit
+  github.com/jesseduffield/lazydocker
+  github.com/pquerna/ffjson
+  github.com/gopherjs/gopherjs
+  github.com/mikefarah/yq/v4
+  github.com/gulyasm/jsonui
+)
+
+for COMMAND in "${COMMANDS[@]}"; do
+  go install -v "${COMMAND}@latest"
+done
+
+#TODO: write yqui based on yq and jsonui
+#TODO: add golang.org/x/vgo
--- unix/install-go-gettable-tools.sh
+++
@@ -1,8 +0,0 @@
-go get -u -v \
-  github.com/jesseduffield/lazygit \
-  github.com/jesseduffield/lazydocker \
-  github.com/pquerna/ffjson \
-  github.com/cheekybits/genny \
-  github.com/gopherjs/gopherjs \
-  golang.org/x/vgo \
-  github.com/gulyasm/jsonui \
