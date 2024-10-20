f28eef0be4a4cd8611f5556cf959b1dfa828f3f7
Daniil Stepanenko <daniil.stepanenko.96@gmail.com>
2024-10-20 20:25:02 +0300

    added cloud/worker/standalone
    Signed-off-by: Daniil Stepanenko <daniil.stepanenko.96@gmail.com>

---
+++ cloud/worker/standalone/setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# shellcheck source=../../../bash.lib.sh
+source "$(dirname "$0")/../../../bash.lib.sh"
+
+TEMPLATE="$(dirname "$0")/cloud-init.yaml"
+CONFIG=/tmp/standalone-cloud-init.yaml
+
+gomplate < "${TEMPLATE}" > "${CONFIG}"
+cloud-init schema --config-file ${CONFIG}
+yq ${CONFIG} | xclip -sel clip
